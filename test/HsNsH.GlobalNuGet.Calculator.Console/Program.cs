3b6a9f0a29403d99d8f867b24415d8fb444afc10
Hasan H. SAHIN <hsnsh@outlook.com>
2022-12-07 00:17:57 +0300

    add test console

---
+++ test/HsNsH.GlobalNuGet.Calculator.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HsNsH.GlobalNuGet.Calculator.Console;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        ICalculator calc = new Calculator();
+
+        System.Console.WriteLine($" 2 + 3 = {calc.Add(2, 3)}");
+    }
+}
